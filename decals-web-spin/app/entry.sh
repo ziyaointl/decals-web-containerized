9c5d87f0e7d228d37155d7254d344c86f62fefb2
Dustin Lang <dstn@cloud2b.arbutus>
2021-01-12 18:10:02 +0000

    use a different uwsgi.ini file

--- decals-web-spin/app/entry.sh
+++ decals-web-spin/app/entry.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export PYTHONPATH=$(pwd)/legacypipe/py:$PYTHONPATH && \
-export PYTHONPATH=$(pwd)/tractor:$PYTHONPATH && \
-export PYTHONPATH=$(pwd)/viewer:$PYTHONPATH && \
+export PYTHONPATH=$(pwd)/legacypipe/py:$PYTHONPATH
+export PYTHONPATH=$(pwd)/tractor:$PYTHONPATH
+export PYTHONPATH=$(pwd)/viewer:$PYTHONPATH
 cd viewer
-uwsgi --stats 127.0.0.1:1717 --touch-reload wsgi.py --ini uwsgi.ini
+uwsgi --stats 127.0.0.1:1717 --touch-reload wsgi.py --ini uwsgi-rancher2.ini
 
 #service nginx start
 #python3 manage.py runserver 0.0.0.0:8000
