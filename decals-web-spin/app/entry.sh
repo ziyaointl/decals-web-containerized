5dd94b305facb8a99739bdb646240084e67e9bc9
Dustin Lang <dstndstn@gmail.com>
2021-01-12 10:08:45 -0500

    uwsgi: stats & uwsgitop

--- decals-web-spin/app/entry.sh
+++ decals-web-spin/app/entry.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 export PYTHONPATH=$(pwd)/legacypipe/py:$PYTHONPATH && \
 export PYTHONPATH=$(pwd)/tractor:$PYTHONPATH && \
-export PYTHONPATH=$(pwd)/decals-web:$PYTHONPATH && \
-cd decals-web
+export PYTHONPATH=$(pwd)/viewer:$PYTHONPATH && \
+cd viewer
+uwsgi --stats 127.0.0.1:1717 --touch-reload wsgi.py --ini uwsgi.ini
+
 #service nginx start
-uwsgi --touch-reload wsgi.py --ini uwsgi.ini
 #python3 manage.py runserver 0.0.0.0:8000
